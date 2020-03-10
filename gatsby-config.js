b50eb073b89d8a60e44f59b9847b29f249fc1e8d
Domeniko <domeniko.divjak@gmail.com>
2020-03-10 20:04:10 +0100

    gatsby-remark-embedder
    gatsby-remark-embedder pa da spucamo kakav insta url

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,12 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+      plugins: [`gatsby-remark-embedder`],
+    },
+  },
+    {
       resolve: 'gatsby-plugin-offline',
       options: {
         runtimeCaching: [
